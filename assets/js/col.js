d46c414432f6cf491dcdbde4e57df8fa1d332861
Tony <37878200+Tonyba@users.noreply.github.com>
2025-11-19 13:39:48 -0400

    removing unnecessary

--- assets/js/col.js
+++
@@ -1,9 +0,0 @@
-const col = document.querySelectorAll('.site-footer-primary-section-4');
-const img = document.createElement('img');
-const p = document.createElement('p');
-
-img.src = 'https://alternateoptionsinc.com/wp-content/uploads/2025/11/Alternate-Options-Web-QR-colors.png';
-p.textContent = 'Share our website below';
-
-col.appendChild(p);
-col.appendChild(img);
